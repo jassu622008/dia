2901413e6fd9718e0cacf9f006048692a73ca81f
jassu622008 <78474277+jassu622008@users.noreply.github.com>
2022-04-03 11:11:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,27 @@ import streamlit as st
 def main():
 	df = pd.read_csv("diabetes.csv")
 
-      X = df[["Glucose", "Insulin", "Age"]]
+      	X = df[["Glucose", "Insulin", "Age"]]
 
-      Y = df[["Outcome"]]
+      	Y = df[["Outcome"]]
 
-      model = DecisionTreeClassifier()
+      	model = DecisionTreeClassifier()
 
-      model.fit(X, Y)
+      	model.fit(X, Y)
 
-      glucose = st.text_input("Enter Glucose Level: ")
+      	glucose = st.text_input("Enter Glucose Level: ")
 
-      Insulin = st.text_input("Enter Insulin Level: ")
+      	Insulin = st.text_input("Enter Insulin Level: ")
 
-      Age = st.text_input("Enter Your Age: ")
+      	Age = st.text_input("Enter Your Age: ")
 
-      pre = model.predict([[int(glucose), int(Insulin), int(Age)]])
+      	pre = model.predict([[int(glucose), int(Insulin), int(Age)]])
 
-      if pre == 1:
-      	st.write("You have diabetes")
+      	if pre == 1:
+      		st.write("You have diabetes")
 
 	else:
-      	st.write("You Does'n have diabetes")
+      		st.write("You Does'n have diabetes")
 
 
 st.write(main())
